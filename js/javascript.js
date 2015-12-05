9ffa32567b40033d34e14829feaf4309e80579eb
CHRIS DIAZ <sicksensetoo@yahoo.com>
2015-12-05 15:57:43 -0800

    update

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+
+/*-------------------------------------
+| nav
+-------------------------------------*/
+	$(function() {
+		var pull		= $('#pull');
+			menu		= $('nav ul');
+			menuheight	= menu.height();
+
+		$(pull).on('click', function(e) {
+			e.preventDefault();
+			menu.slideToggle();
+		});
+	});
+
+	$(window).resize(function() {
+		var w = $(window).width();
+		if(w > 320 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+
+	});
+
+
+
+}); /*here ends doc ready*/
